1d1af68fb0a71fa662a9ffca0945dd78cc3ae238
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 01:44:26 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,27 @@
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+def generate_random_data():
+import random
+    data = generate_random_data()
+
+    main()
+
+
+    return data
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def main():
+    data = generate_random_data()
+    main()
+
+
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+def generate_random_data():
+    for item in data:
+    return data
